6e103bd5afde262e3e59462d0644e0689eaff3af
Jakub Ziolkowski <jakubziolkowski@digiproject.uk>
2024-12-03 22:20:35 +0100

    Create OrtoDataComparisons Method Implemented

---
+++ DiGi.GIS.Emgu.CV/Classes/OrtoDataComparison.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiGi.GIS.Emgu.CV.Classes
+{
+    public class OrtoDataComparison : Core.Parameter.Classes.ParametrizedObject, Interfaces.IGISObject
+    {
+        public OrtoDataComparison() 
+        {
+            
+        }
+    }
+}
---
+++ DiGi.GIS.Emgu.CV/Create/Mat.cs
@@ -0,0 +1,29 @@
+﻿using DiGi.GIS.Classes;
+using Emgu.CV;
+using System;
+
+namespace DiGi.GIS.Emgu.CV
+{
+    public static partial class Create
+    {
+        public static Mat Mat(this OrtoDatas ortoDatas, DateTime dateTime)
+        {
+            if(ortoDatas == null)
+            {
+                return null;
+            }
+
+            byte[] bytes = ortoDatas.GetBytes(dateTime);
+            if(bytes == null)
+            {
+                return null;
+            }
+
+            Mat result = new Mat();
+            CvInvoke.Imdecode(bytes, global::Emgu.CV.CvEnum.ImreadModes.Color, result);
+
+            return result;
+        }
+    }
+}
+
